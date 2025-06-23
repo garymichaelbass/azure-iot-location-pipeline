4443ffc9e7010ece418dc8b11edcb5d9488f9185
garymichaelbass <gary@garymichaelbass.com>
2025-06-23 12:50:08 -0500

    Update databricks conditional using a try block

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -57,17 +57,14 @@ provider "azuread" {
 provider "databricks" {
   alias = "workspace"
 
-  # Use a conditional to avoid crashing when workspace values aren't available yet
-  host = (
-    can(azurerm_databricks_workspace.iot_databricks_workspace.workspace_url)
-    ? azurerm_databricks_workspace.iot_databricks_workspace.workspace_url
-    : null
+  host = try(
+    azurerm_databricks_workspace.iot_databricks_workspace.workspace_url,
+    null
   )
 
-  azure_workspace_resource_id = (
-    can(azurerm_databricks_workspace.iot_databricks_workspace.id)
-    ? azurerm_databricks_workspace.iot_databricks_workspace.id
-    : null
+  azure_workspace_resource_id = try(
+    azurerm_databricks_workspace.iot_databricks_workspace.id,
+    null
   )
 }
 
