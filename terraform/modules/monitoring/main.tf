9b073a72dee5152cc7640231f4c0734ad086d5b4
garymichaelbass <gary@garymichaelbass.com>
2025-06-28 20:34:10 -0500

    modules/monitoring/main.tf Corrected typo

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -25,13 +25,8 @@ resource "azurerm_role_assignment" "grafana_monitor_reader" {
   principal_id         = azurerm_dashboard_grafana.iot_grafana.identity[0].principal_id
 }
 
-
 # Grant your user Grafana Admin permissions to the Azure Managed Grafana instance
 # principal_id is the output of "az ad signed-in-user show --query id -o tsv"
-184de8f4-c1e8-48b7-81a6-bbd9e875b94a
-
-
-# Grant your user Grafana Admin permissions to the Azure Managed Grafana instance
 resource "azurerm_role_assignment" "grafana_admin_for_user" {
   scope                = azurerm_dashboard_grafana.iot_grafana.id
   role_definition_name = "Grafana Admin"
