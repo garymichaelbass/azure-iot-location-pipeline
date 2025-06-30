e2f445d07546eddb47c9121f3da47ee15d91d20a
garymichaelbass <gary@garymichaelbass.com>
2025-06-29 19:38:55 -0500

    Adding commands to debug databricks as portal.azure.com is not showing pertinent info.

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -24,7 +24,7 @@ resource "azurerm_kubernetes_cluster" "iot_aks_cluster" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_B2ms"
     temporary_name_for_rotation = "tmpnodepool1"
   }
